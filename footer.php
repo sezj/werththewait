26b7e5070a4da7b82b9b1ef7b58d91b1aee1d80d
sezj <sarahzachrich@gmail.com>
2015-09-04 16:54:58 -0400

    full page background image

--- footer.php
+++ footer.php
@@ -18,13 +18,22 @@
 						)); ?>
 					</nav>
 
-					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</p>
+					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> University of Florida College of Engineering</p>
 
 				</div>
 
 			</footer>
 
+
+
 		</div>
+
+		<?php if ( is_page_template( 'page-landing.php' ) ) { ?>
+			<div class="bodybg"> 
+				<img src="<?php echo get_template_directory_uri(); ?>/library/images/background-photo.jpg" src-set="<?php echo get_template_directory_uri(); ?>/library/images/background-photo-mobile-devices.jpg 500w, <?php echo get_template_directory_uri(); ?>/library/images/background-photo.jpg 1000w, <?php echo get_template_directory_uri(); ?>/library/images/background-photo.jpg 2000w" alt="" /> 
+				<!-- <img src="<?php echo get_template_directory_uri(); ?>/library/images/background-photo-mobile-devices.jpg" alt="" />  -->
+			</div> 
+		<?php } ?>
 
 		<?php // all js scripts are loaded in library/bones.php ?>
 		<?php wp_footer(); ?>
